cfec6e21dccce5ae47bb1e576ae9611afeaa906f
trongbang86 <trongbang86@gmail.com>
2017-06-21 04:32:16 +0100

    removed duplicated messages on the console

--- src/main/java/sample/logback/SampleLogbackApplication.java
+++ src/main/java/sample/logback/SampleLogbackApplication.java
@@ -22,6 +22,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ansi.AnsiOutput;
+import org.springframework.boot.ansi.AnsiOutput.Enabled;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
